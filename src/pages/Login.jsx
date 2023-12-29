3a6735f147ec0d318dfe630c1e1aa372748c8b1c
GunPKate <gunPK>
2023-12-29 21:41:04 +0700

    clean login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,7 +4,7 @@ function Login(){
  
         <div class="login-box">
         <div class="login-logo">
-            <a href="../../index2.html"><b>Admin</b>LTE</a>
+            <b>.NET</b><span> POS Point of Sale</span>
         </div>
 
         <div class="card">
@@ -27,28 +27,13 @@ function Login(){
                     </div>
                 </div>
                 </div>
-                <div class="row">
-                <div class="col-8">
-                    <div class="icheck-primary">
-                    <input type="checkbox" id="remember"/>
-                    <label for="remember">
-                        Remember Me
-                    </label>
+                <div class="row justify-content-center">
+                    <div class="col-4 ">
+                        <button type="submit" class="btn btn-primary btn-block">Sign In</button>
                     </div>
-                </div>
-
-                <div class="col-4">
-                    <button type="submit" class="btn btn-primary btn-block">Sign In</button>
-                </div>
 
                 </div>
             </form>
-            <p class="mb-1">
-                <a href="forgot-password.html">I forgot my password</a>
-            </p>
-            <p class="mb-0">
-                <a href="register.html" class="text-center">Register a new membership</a>
-            </p>
             </div>
         </div>
         </div>
