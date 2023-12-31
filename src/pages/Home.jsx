fbe413308344661f53ade7ba0ec48de70ac40017
GunPKate <gunPK>
2023-12-31 12:38:22 +0700

    navbar and sidebar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,12 @@
+import Sidebar from "../components/Sidebar";
+import Navbar from "../components/Navbar";
+
 function Home() {
     return (<>
-        <h3>Welcome to home</h3>
+        <div className="wrapper">
+            <Navbar/>
+            <Sidebar/>
+        </div>
     </>)
 }
 
