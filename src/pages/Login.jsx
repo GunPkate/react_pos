51b4dbecff4bfbf5b01f257dccf5eef7b9ff6f33
GunPKate <gunPK>
2023-12-31 11:31:12 +0700

    login function

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,30 @@
+import axios from "axios";
+import { useState } from "react";
+import Swal from "sweetalert2";
+
 function Login(){
+    const [userName,setUser] = useState('');
+    const [password,setPassword] = useState('');
+
+    const handleLogin = async () => {
+        try {
+
+            await axios.post("https://localhost:7106/api/User/Login",{usr:userName,pwd:password}).then(res=>{
+                if(res.data.message === 'success'){
+                    localStorage.setItem('token',res.data.token)
+                }
+            }).catch(err =>{
+                throw err.response.data;
+            })
+        }
+        catch (e) {
+            Swal.fire({
+                title: 'error',
+                message: e.message,
+                icon: 'error'
+            })
+        }
+    }
     return (
     <>
  
@@ -10,9 +36,9 @@ function Login(){
         <div class="card">
             <div class="card-body login-card-body">
             <p class="login-box-msg">Sign in to start your session</p>
-            <form>
+            <form onSubmit={handleLogin}>
                 <div class="input-group mb-3">
-                <input type="email" class="form-control" placeholder="Email"/>
+                <input onChange={e => setUser(e.target.value)} type="email" class="form-control" placeholder="Email"/>
                 <div class="input-group-append">
                     <div class="input-group-text">
                     <span class="fas fa-envelope"></span>
@@ -20,7 +46,7 @@ function Login(){
                 </div>
                 </div>
                 <div class="input-group mb-3">
-                <input type="password" class="form-control" placeholder="Password"/>
+                <input onChange={e => setPassword(e.target.value)} type="password" class="form-control" placeholder="Password"/>
                 <div class="input-group-append">
                     <div class="input-group-text">
                     <span class="fas fa-lock"></span>
@@ -29,7 +55,7 @@ function Login(){
                 </div>
                 <div class="row justify-content-center">
                     <div class="col-4 ">
-                        <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+                        <button onClick={handleLogin} type="submit" class="btn btn-primary btn-block">Sign In</button>
                     </div>
 
                 </div>
