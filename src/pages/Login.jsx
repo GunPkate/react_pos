e76fc4f17133e0e90900bcfe175e44e4243a426e
GunPKate <gunPK>
2023-12-31 19:39:02 +0700

    log out and remove unused

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -32,34 +32,34 @@ function Login(){
     return (
     <>
  
-        <div class="login-box">
-        <div class="login-logo">
+        <div className="login-box">
+        <div className="login-logo">
             <b>.NET</b><span> POS Point of Sale</span>
         </div>
 
-        <div class="card">
-            <div class="card-body login-card-body">
-            <p class="login-box-msg">Sign in to start your session</p>
+        <div className="card">
+            <div className="card-body login-card-body">
+            <p className="login-box-msg">Sign in to start your session</p>
             <form onSubmit={handleLogin}>
-                <div class="input-group mb-3">
-                <input onChange={e => setUser(e.target.value)} type="email" class="form-control" placeholder="Email"/>
-                <div class="input-group-append">
-                    <div class="input-group-text">
-                    <span class="fas fa-envelope"></span>
+                <div className="input-group mb-3">
+                <input onChange={e => setUser(e.target.value)} type="email" className="form-control" placeholder="Email"/>
+                <div className="input-group-append">
+                    <div className="input-group-text">
+                    <span className="fas fa-envelope"></span>
                     </div>
                 </div>
                 </div>
-                <div class="input-group mb-3">
-                <input onChange={e => setPassword(e.target.value)} type="password" class="form-control" placeholder="Password"/>
-                <div class="input-group-append">
-                    <div class="input-group-text">
-                    <span class="fas fa-lock"></span>
+                <div className="input-group mb-3">
+                <input onChange={e => setPassword(e.target.value)} type="password" className="form-control" placeholder="Password"/>
+                <div className="input-group-append">
+                    <div className="input-group-text">
+                    <span className="fas fa-lock"></span>
                     </div>
                 </div>
                 </div>
-                <div class="row justify-content-center">
-                    <div class="col-4 ">
-                        <button onClick={handleLogin} type="submit" class="btn btn-primary btn-block">Sign In</button>
+                <div className="row justify-content-center">
+                    <div className="col-4 ">
+                        <button onClick={handleLogin} type="submit" className="btn btn-primary btn-block">Sign In</button>
                     </div>
 
                 </div>
