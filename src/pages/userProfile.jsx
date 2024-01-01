64215cc5fc48603933921544b17e761181c6b572
GunPKate <gunPK>
2024-01-01 15:18:28 +0700

    update user profile function

--- src/pages/userProfile.jsx
+++ src/pages/userProfile.jsx
@@ -1,23 +1,79 @@
-import Sidebar from "../components/Sidebar";
-import Navbar from "../components/Navbar";
 import Template from "../components/Template";
+import { useState,useEffect } from "react";
+import Swal from "sweetalert2";
+import axios from "axios";
+import Config from "../config";
 
 function UserProfile(){
+    const[name,setName] = useState('');
+    const[usr,setUsr] = useState('');
+    const[pwd,setPwd] = useState('');
+
+    useEffect(()=>{ fetchData() },[])
+
+    const fetchData = async () =>{
+        try {
+            
+            await axios.post(Config.api+"/api/User/GetInfo",null,Config.headers).then(res=>{
+                console.log(res)
+                if(res.data)
+                    setName(res.data.name);
+                    setUsr(res.data.usr);
+            }).catch(err =>{
+                throw err.response.data;
+            })
+        }
+        catch (e) {
+            Swal.fire({
+                title: 'error',
+                message: e.message,
+                icon: 'error'
+            })
+        }
+    }
+
+    const handleSave = async (e) =>{
+        e.preventDefault();
+        try {
+            const params = {
+                name: name,
+                usr: usr,
+                pwd: pwd
+            }
+            await axios.post(Config.api+'/api/User/ChangeProfileSave',params,Config.headers).then(res => {
+                if(res){
+                    Swal.fire({
+                        title: 'Save New User',
+                        icon: 'success',
+                        timer: 1500
+                    })
+                }
+            })
+        } catch (error) {
+            Swal.fire({
+                title: 'error',
+                text: e.message,
+                icon: 'error'
+            })
+        }
+    }
     return (<>
             <Template>
                 <div className="h5 pt-4">Profile</div>
                 <div className="card">
                     <div className="card-body">
                         <div>Name</div>
-                        <input className="form-control" type="text"/>
-                    </div>
-                    <div className="card-body">
+                        <input value={name} onChange={e => setName(e.target.value)} className="form-control" type="text"/>
+
                         <div>User</div>
-                        <input className="form-control" type="text"/>
-                    </div>
-                    <div className="card-body">
+                        <input value={usr} onChange={e => setUsr(e.target.value)}  className="form-control" type="text"/>
+
                         <div>Password</div>
-                        <input className="form-control" type="text"/>
+                        <input onChange={e => setPwd(e.target.value)}  className="form-control" type="password"/>
+                    
+                        <button onClick={handleSave} className="btn-primary nt-3">
+                            Save
+                        </button>
                     </div>
                 </div>
             </Template>
